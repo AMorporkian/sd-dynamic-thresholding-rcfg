2992c290592df30a3d4a95e2cad5f1ad80292193
Pat Shanahan <AMorporkian@users.noreply.github.com>
2023-06-13 22:51:24 -0500

    Update dynthres_core.py

--- dynthres_core.py
+++ dynthres_core.py
@@ -3,10 +3,10 @@ class RescaleCFG:
     def __init__(self, phi):
         self.phi = phi
 
-    def dynthresh(self, cond, uncond, cfgScale, weights):
-        x_cfg = uncond + cond_scale * (cond - uncond)
+    def dynthresh(self, cond, uncond, cfg, weights):
+        x_cfg = uncond + cfg * (cond - uncond)
         ro_pos = torch.std(cond, dim=(1,2,3), keepdim=True)
         ro_cfg = torch.std(x_cfg, dim=(1,2,3), keepdim=True)
         x_rescaled = x_cfg * (ro_pos / ro_cfg)
-        return multiplier * x_rescaled + (1.0 - self.phi) * x_cfg
+        return self.phi * x_rescaled + (1.0 - self.phi) * x_cfg
         
